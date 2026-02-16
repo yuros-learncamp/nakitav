e1a5ba7d20f1782370b0bf2bf3ab82b9bc8f4d6d
Fathi Rakha Herlambang <fathirakha.au22@gmail.com>
2026-02-16 13:25:02 +0700

    Add files via upload

---
+++ session-1/submit the assignment/rokets_roda.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	
+	const double PI = 3.141592653589793;
+
+	double diameter_ban_cm;
+	double jarak_keseluruhan;
+
+	//input data ban
+	cout << "Masukan Diameter Ban (cm) : ";
+	cin >> diameter_ban_cm ;
+
+	cout << "Jarak Keseluruhan (km) : ";
+	cin >> jarak_keseluruhan;
+
+	// hitungan keliling
+	double keliling_ban_cm = PI * diameter_ban_cm;
+
+	// konver km ke cm
+	double jarak_keseluruhan_cm = jarak_keseluruhan * 100000;
+
+	//hitungan akhir
+	double total_putaran = jarak_keseluruhan_cm / keliling_ban_cm;
+
+	//total 
+	cout << "\nTotal putaran ban:  " << total_putaran << endl;
+		return 0;
+}
+
+
+
+
+
+
